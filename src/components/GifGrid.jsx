9cbde60e51eeae4a80c9256ab0c64c511e957e3c
Johan Cordoba <johan.cordoba@globant.com>
2023-11-13 20:22:32 -0500

    Feat: Add GifItem to show the image

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+
+import { GifItem } from './GifItem';
+
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
@@ -16,13 +19,11 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
-      <ol>
-        {
-            images.map(({id, title}) => (
-                <li key={id}>{title}</li>
-            ))
-        }
-      </ol>
+      <div className='card-grid'>
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
+      </div>
     </>
   );
 };
